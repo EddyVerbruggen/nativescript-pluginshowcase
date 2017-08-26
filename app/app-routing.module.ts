f4679daf76b22844cec36b3df0560fe4312c8a16
EddyVerbruggen <eddyverbruggen@gmail.com>
2017-08-26 11:45:09 +0200

    Router module preloading

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from "@angular/core";
-import { Routes } from "@angular/router";
+import { PreloadAllModules, Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 const routes: Routes = [
   {path: "", redirectTo: "/menu", pathMatch: "full"},
-  // {path: "home", loadChildren: "./home/home.module#HomeModule"},
   {path: "menu", loadChildren: "./menu.module#MenuModule"}
 ];
 
 @NgModule({
-  imports: [NativeScriptRouterModule.forRoot(routes)],
+  imports: [NativeScriptRouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [NativeScriptRouterModule]
 })
 export class AppRoutingModule {
