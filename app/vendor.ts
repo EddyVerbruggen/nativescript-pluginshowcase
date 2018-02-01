788d4e77cebc8c4d85545e20cfcc98b08e28bb33
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-02-01 18:59:28 +0100

    Webpack optimization

--- app/vendor.ts
+++ app/vendor.ts
@@ -18,3 +18,20 @@ require("@angular/router");
 require("nativescript-angular/platform-static");
 require("nativescript-angular/forms");
 require("nativescript-angular/router");
+
+// required by nativescript-angular
+require("@angular/compiler");
+
+require("nativescript-angular/http");
+require("nativescript-angular/dom-adapter");
+require("nativescript-angular/schema-registry");
+require("nativescript-angular/resource-loader");
+require("nativescript-angular/modal-dialog");
+
+// see https://github.com/telerik/nativescript-ui-feedback/issues/195
+require("nativescript-pro-ui/sidedrawer");
+
+// move to the vendor chunk as this is required in every individual module
+require("nativescript-angular/nativescript.module");
+
+require("nativescript-ngx-fonticon");
