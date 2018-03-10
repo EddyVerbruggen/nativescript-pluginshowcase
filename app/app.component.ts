c8b676734f8e9aad58e59c6379ada8964deae2cf
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-03-10 15:02:41 +0100

    iPhone X compat for menu footer #8

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import * as application from "tns-core-modules/application";
+import { isIOS } from "tns-core-modules/platform";
 
 @Component({
-    selector: "ns-app",
-    templateUrl: "app.component.html"
+  selector: "ns-app",
+  templateUrl: "app.component.html"
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    // Only 1 thingy needs an iPhone X (and other devices later as well, perhaps) tweak, so doing it quick & dirty here
+    if (isIOS && application.ios.window.safeAreaInsets) {
+      const bottomSafeArea: number = application.ios.window.safeAreaInsets.bottom;
+      if (bottomSafeArea > 0) {
+        application.addCss(`
+            .madeby-container Label.madeby-text { padding-bottom: ${bottomSafeArea} }
+          `);
+      }
+    } else {
+      application.addCss(`
+            .madeby-container Label.madeby-text { padding-bottom: 14 }
+          `);
+    }
+  }
+}
