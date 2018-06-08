3f5dc1fa3ca350b731182c982e1e54784988acf4
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-06-08 09:58:29 +0200

    Replace nativescript-gradient with the new built-in gradients #19

--- app/shared/config.ts
+++ app/shared/config.ts
@@ -1,5 +1,5 @@
 export class Config {
-  static isProdMode: boolean = false;
+  static isProdMode: boolean = true;
   static isTablet: boolean = false; // set in app.module.ts
 
   static DEBUG_MODE = {
