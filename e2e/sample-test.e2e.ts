a120ed046b5a1af3cf2036f6f6020054e796267e
vhristov5555 <vasil.hristov@progress.com>
2019-08-02 18:38:54 +0300

    update tests.

--- e2e/sample-test.e2e.ts
+++ e2e/sample-test.e2e.ts
@@ -1,4 +1,4 @@
-import { AppiumDriver, createDriver, SearchOptions } from "nativescript-dev-appium";
+import { AppiumDriver, createDriver, SearchOptions, nsCapabilities } from "nativescript-dev-appium";
 import { assert } from "chai";
 
 describe("smoke test", () => {
@@ -6,7 +6,7 @@ describe("smoke test", () => {
 
   async function openMenuItem(which: string, verifyByLabel: string) {
     // The Chinese fella below is the hamburger icon (found it by running "tns debug android" and inspecting it with Chrome)
-    const menuButton = await driver.findElementByText("", SearchOptions.contains);
+    const menuButton = await driver.findElementByText("", SearchOptions.contains, 10000);
     await menuButton.click();
     const messageLabel = await driver.findElementByText(which, SearchOptions.contains);
     await messageLabel.click();
@@ -19,66 +19,75 @@ describe("smoke test", () => {
     });
   }
 
-  before(async () => {
+  before(async function () {
+    nsCapabilities.testReporter.context = this;
     driver = await createDriver();
   });
 
-  after(async () => {
+  after(async function () {
     await driver.quit();
   });
 
   afterEach(async function () {
     if (this.currentTest.state === "failed") {
-      await driver.logScreenshot(this.currentTest.title);
+      await driver.logTestArtifacts(this.currentTest.title);
     }
   });
 
-  it("should find the welcome text", async () => {
+  it("should find the welcome text", async function () {
     const displayMsg = "Thanks for installing this app,";
     const messageLabel = await driver.findElementByText(displayMsg, SearchOptions.exact);
     assert.equal(await messageLabel.text(), displayMsg);
   });
 
-  // it("screenshot comparison succeeds", async () => {
+  // it("screenshot comparison succeeds", async function(){
   //   const isDisplayMessageCorrect = await driver.compareScreen("hello-world-display.png", 10, 0.2);
   //   assert.isTrue(isDisplayMessageCorrect, "Look at hello-world-display-diif.png");
   // });
 
-  it("navigate to feedback", async () => {
+  it("navigate to feedback", async function () {
     await openMenuItem("Feedback", "Toast");
-    const shortToastButton = await driver.findElementByText("Short", SearchOptions.exact);
+    const shortToastButton = await driver.waitForElement("Short");
     await shortToastButton.click();
 
-    const feedbackSuccessButton = await driver.findElementByText("Success, 2.5s", SearchOptions.exact);
+    const feedbackSuccessButton = await driver.waitForElement("Success, 2.5s");
     await feedbackSuccessButton.click();
-    // the message needs to go, so wait a little
-    await wait(3500);
 
-    const fancyAlertSuccessButton = await driver.findElementByText("Success", SearchOptions.exact);
-    const fancyAlertButtonText: string = await fancyAlertSuccessButton.text();
-    assert.equal(fancyAlertButtonText.toLowerCase(), "success");
+    const successPeekABoo = driver.isIOS ? "Success, Peek-a-boo!" : "Peek-a-boo!";
+
+    let fancyAlertButtonText = await (await driver.waitForElement(successPeekABoo)).text();
+    assert.equal(fancyAlertButtonText.toLowerCase(), successPeekABoo.toLowerCase());
+
+    const sorryTextAlert = "Sorry. I will dismiss myself after 2.5 seconds.";
+    fancyAlertButtonText = await (await driver.waitForElement(sorryTextAlert)).text();
+    assert.equal(fancyAlertButtonText.toLowerCase(), sorryTextAlert.toLowerCase());
+
+    if (await (await driver.waitForElement(successPeekABoo)).isDisplayed()) {
+      await driver.sleep(4000);
+    }
   });
 
-  it("navigate to input", async () => {
+  it("navigate to input", async function () {
     await openMenuItem("Input", "Drawing");
-    const checkboxTabButton = await driver.findElementByText("Checkbox", SearchOptions.exact);
+    const checkboxTabButton = await driver.waitForElement("Checkbox");
     await checkboxTabButton.click();
 
     // checkboxes
-    (await driver.findElementByText("React Native", SearchOptions.exact)).click();
-    (await driver.findElementByText("NativeScript", SearchOptions.exact)).click();
-    (await driver.findElementByText("React Native", SearchOptions.exact)).click();
+    await (await driver.waitForElement("React Native")).click();
+    await (await driver.waitForElement("NativeScript")).click();
+    await (await driver.waitForElement("React Native")).click();
 
     // radiobuttons
-    (await driver.findElementByText("React Native!", SearchOptions.exact)).click();
-    (await driver.findElementByText("NativeScript!", SearchOptions.exact)).click();
+    await (await driver.waitForElement("React Native!")).click();
+    await (await driver.waitForElement("NativeScript!")).click();
   });
 
-  it("navigate to app icon", async () => {
-    await openMenuItem("App icon", "App shortcuts");
-    const displayMsg = "add deeplink to mapping";
-    const buttonLabel = await driver.findElementByText(displayMsg, SearchOptions.exact);
+  it("navigate to app icon", async function () {
+    const textValue = driver.isIOS ? "App Shortcuts" : "App shortcuts";
+    await openMenuItem("App icon", textValue);
+    const displayMsg = driver.isIOS ? "Add deeplink to Mapping" : "ADD DEEPLINK TO MAPPING";
+    const buttonLabel = await driver.waitForElement(displayMsg);
     const buttonText: string = await buttonLabel.text();
-    assert.equal(buttonText.toLowerCase(), displayMsg);
+    assert.equal(buttonText, displayMsg);
   });
 });
--- e2e/setup.ts
+++ e2e/setup.ts
@@ -1,9 +1,18 @@
-import { startServer, stopServer } from "nativescript-dev-appium";
+import { startServer, stopServer, ITestReporter, nsCapabilities, LogImageType } from "nativescript-dev-appium";
+const addContext = require('mochawesome/addContext');
 
-before("start server", async () => {
-  await startServer();
+const testReporterContext = <ITestReporter>{};
+testReporterContext.name = "mochawesome";
+testReporterContext.reportDir = "mochawesome-report";
+testReporterContext.log = addContext;
+testReporterContext.logImageTypes = [LogImageType.screenshots];
+nsCapabilities.testReporter = testReporterContext;
+
+before("start server", async function () {
+    nsCapabilities.testReporter.context = this;
+    await startServer();
 });
 
-after("stop server", async () => {
-  await stopServer();
+after("stop server", async function () {
+    await stopServer();
 });
